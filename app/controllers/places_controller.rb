a5f804b38e9b8855740f040d6cfc65d1d472d8fe
Jessica Brady <jessicabrady16@gmail.com>
2019-07-31 14:04:09 -0600

    connected users to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class PlacesController < ApplicationController
-    def index
-        @places = Place.all.page(params[:page]).per(5)
-    end
+  before_action :authenticate_user!, only: %i[new create]
 
-    def new
-        @place = Place.new
-    end
+  def index
+    @places = Place.all.page(params[:page]).per(5)
+  end
 
-    def create
-        Place.create(place_params)
-        redirect_to root_path
-    end
+  def new
+    @place = Place.new
+  end
 
-    private
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
 
-    def place_params
-        params.require(:place).permit(:name, :description, :address)
-    end
+  private
 
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
-
