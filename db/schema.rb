77ff095daf1f37c3285484cb6c0e69fffd45eb16
Jessica Brady <jessicabrady16@gmail.com>
2019-08-07 23:43:15 -0600

    fixing db migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_232932) do
+ActiveRecord::Schema.define(version: 2019_08_08_053236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
