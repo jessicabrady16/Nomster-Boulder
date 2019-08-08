c884fafd450e452f1cd71214a2bacb090a1c10e5
Jessica Brady <jessicabrady16@gmail.com>
2019-08-07 22:38:08 -0600

    added map to place page and restricted map view to validated geocode

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'Boulder Rock Club', address: '2829 Mapleton Ave, Boulder, CO 80301', description: 'Great indoor rock climbing. Offers yoga and other training programs for all ages.')
+
+Place.create(name: 'Mount Sanitas and Sanitas Valley Loop Trails', address: '501 Sunshine Canyon Dr, Boulder, CO 80302', description: 'Mount Sanitas and Sanitas Valley Loop Trail is a 3.2 mile heavily trafficked loop trail located near Boulder, Colorado that features beautiful wild flowers and is rated as difficult.')
+
+Place.create(name: 'Celestial Seasonings', address: '4600 Sleepytime Dr, Boulder, CO 80301', description: 'Run by a well-known tea company, this cheery weekday cafe serves casual breakfast & lunch favorites as well as gives tours.')
+
+Place.create(name: 'Eldorado Canyon State Park', address: '9 Kneale Rd, Eldorado Springs, CO 80025', description: 'Eldorado Canyon State Park is part of the Colorado State Park system. It is located in Boulder County near the city of Boulder. The park consists of two areas, the Inner Canyon and Crescent Meadows. The park encompasses 885 acres with a variety of recreation opportunities available.')
+
+Place.create(name: 'Eldora', address: ' 2861 Eldora Ski Rd, Nederland, CO 80466', description: 'Eldora Mountain Resort is a ski area in the southwest corner of Boulder County, Colorado, near the unincorporated community of Eldora and 3 miles west of the town of Nederland.')
+
+Place.create(name: 'Mountain Sun Pub & Brewery', address: '1535 Pearl St, Boulder, CO 80302', description: 'Craft beers, brewery tours & pub grub plus folk, acoustic & bluegrass music some nights.')
+
+Place.create(name: 'Heifer and the Hen', address: '5290 Arapahoe Ave, Boulder, CO 80303', description: 'Artisan creamery dishing up innovative scoops, floats & more in a bright, mod shop with swing seats.')
+
+Place.create(name: 'Leaf', address: '1710 Pearl St, Boulder, CO 80302', description: 'Family-owned eatery offering sustainable, farm-to-table vegetarian & vegan meals in a relaxed space.')
+
+Place.create(name: 'Ku Cha House of Tea', address: '5201, 1211 Pearl St, Boulder, CO 80302', description: 'Casual spot for exotic teas from around the world plus the option to blend your own leaves.')
+
+Place.create(name: 'The Boulder Dushanbe Teahouse', address: '1770 13th St, Boulder, CO 80302', description: 'Ornate landmark shipped piece by piece from Tajikistan & rebuilt creekside, with teas & light bites.')
+
+Place.create(name: 'Sherpa Restaurant', address: '825 Walnut St, Boulder, CO 80302', description: 'Dumplings, noodles & even yak curry, plus lunch specials & a full bar, amid Himalayan folk art and large outdoor seating.')
+
+Place.create(name: 'OZO Coffee', address: '5340 Arapahoe Ave, Boulder, CO 80303', description: 'Carefully sourced, house-roasted beans in the cup & bagged to go, plus treats, at this buzzing cafe.')
+
+Place.create(name: 'Eben G. Fine Park', address: '101 Arapahoe Ave, Boulder, CO 80302', description: 'Green space with a playground, grassy areas & picnic facilities, river access for water sports including tubing, plus access to trails that follow the Boulder Creek.')
+
+Place.create(name: 'Chautauqua', address: 'Baseline Rd & 9th St, Boulder, CO 80302', description: 'Hilltop Chautauqua is an affluent area overlooking downtown Boulder, known for tree-lined streets dotted with Victorian homes. Chautauqua Park is a spacious greenway containing cottages and a dining hall with mountain views. Summer concerts take place in the park’s Chautauqua Auditorium, set in a barn-like hall built in 1898. Hiking trails branch out from the Chautauqua Trailhead, climbing ridges & Flatirons.')
+
+Place.create(name: 'Boulder Theater', address: '2032 14th St, Boulder, CO 80302', description: 'Art deco institution since 1906 for various music shows & mostly avant-garde films.')
+
+Place.create(name: 'Fiske Planetarium', address: ' 2414 Regent Dr, Boulder, CO 80309', description: 'Fiske Planetarium and Science Center is a public museum dedicated to educating the public on astronomy and astrophysics. It is a constituent of the Department of Astrophysical and Planetary Sciences at the University of Colorado Boulder, located in Boulder, Colorado.')
