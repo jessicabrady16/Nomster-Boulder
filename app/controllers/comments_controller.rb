448306f10707ab7ca9a6bfc27514c998958a1325
Jessica Brady <jessicabrady16@gmail.com>
2019-08-10 16:50:00 -0600

    sytling for photo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
     end
   end
    
-               def destroy  
+    def destroy  
     @place = Place.find(params[:place_id])
     @comment= Comment.find(params[:id])
     if @comment.user != current_user
