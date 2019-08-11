703a7c97a40b63bd83ed1968be52b1f787a7a3ef
Jessica Brady <jessicabrady16@gmail.com>
2019-08-11 13:16:28 -0600

    worked on photo for index and styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
 
-  
+
   def create
     @place = Place.find(params[:place_id])
     @comment= @place.comments.create(comment_params.merge(user: current_user))
     if @comment.valid?
     redirect_to place_path(@place)
-    else 
+    else
 
       return render plain: ' Be kind! Please rewind and enter a comment AND/OR a rating! We cannot "Nom" air!', status: :forbidden
-      #render :new, status: :unprocessable_entity 
-      
+      #render :new, status: :unprocessable_entity
+
     end
   end
-   
-    def destroy  
+
+    def destroy
     @place = Place.find(params[:place_id])
     @comment= Comment.find(params[:id])
     if @comment.user != current_user
@@ -28,7 +28,7 @@ end
 
 
 
-private 
+private
 
 def comment_params
     params.require(:comment).permit(:message, :rating)
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,6 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user!
   
-
-
     def create
         @place = Place.find(params[:place_id])
         @photo= @place.photos.create(photo_params.merge(user: current_user))
