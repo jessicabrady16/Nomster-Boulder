40c9f4324c5a37d274014ec2c340ca65d6338d47
Jessica Brady <jessicabrady16@gmail.com>
2019-08-14 13:17:58 -0600

    added sendgrid for mailing in production about comment additions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,35 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @comment= @place.comments.create(comment_params.merge(user: current_user))
+    @comment = @place.comments.create(comment_params.merge(user: current_user))
     if @comment.valid?
-    redirect_to place_path(@place)
+      redirect_to place_path(@place)
     else
 
       return render plain: ' Be kind! Please rewind and enter a comment AND/OR a rating! We cannot "Nom" air!', status: :forbidden
-      #render :new, status: :unprocessable_entity
+            # render :new, status: :unprocessable_entity
 
           end
         end
 
-    def destroy
+  def destroy
     @place = Place.find(params[:place_id])
-    @comment= Comment.find(params[:id])
+    @comment = Comment.find(params[:id])
     if @comment.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
   end
+
     @comment.destroy
     redirect_to place_path(@place)
-
 end
 
+  private
 
-
-private
-
-def comment_params
+  def comment_params
     params.require(:comment).permit(:message, :rating)
-end
-
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,49 +1,46 @@
+# frozen_string_literal: true
 
 class MyValidator < ActiveModel::Validator
   def validate(comment)
-
-    if comment.message.present?  || comment.rating.present?
-      puts "OK" + 'msg:' + comment.message + 'rating:' + comment.rating
+    if comment.message.present? || comment.rating.present?
+      puts 'OK' + 'msg:' + comment.message + 'rating:' + comment.rating
       return
 
     end
 
-    if !comment.message.present?
-      puts "missing message"
-            comment.errors[:message] << 'Please add a message to "Nom" on!'
+    unless comment.message.present?
+      puts 'missing message'
+      comment.errors[:message] << 'Please add a message to "Nom" on!'
     end
 
-    if !comment.rating.present?
-      puts "missing rating"
+    unless comment.rating.present?
+      puts 'missing rating'
       comment.errors[:rating] << 'Please add at least a rating for us to "Nom" on!'
       end
   end
 end
 
-
-
 class Comment < ApplicationRecord
   include ActiveModel::Validations
   validates_with MyValidator
 
+  belongs_to :user
+  belongs_to :place
+  after_create :send_comment_email
 
-    belongs_to :user
-    belongs_to :place
-    after_create :send_comment_email
+  RATINGS = {
+    'One Star': '1_star',
+    'Two Stars': '2_stars',
+    'Three Stars': '3_stars',
+    'Four Stars': '4_stars',
+    'Five Stars': '5_stars'
+  }.freeze
 
-    RATINGS = {
-        'One Star': '1_star',
-        'Two Stars': '2_stars',
-        'Three Stars': '3_stars',
-        'Four Stars': '4_stars',
-        'Five Stars': '5_stars'
-      }
+  def humanized_rating
+    RATINGS.invert[rating]
+  end
 
-      def humanized_rating
-        RATINGS.invert[self.rating]
-      end
-
-      def send_comment_email
-        NotificationMailer.comment_added(self).deliver_now
-      end
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver_now
+  end
 end
