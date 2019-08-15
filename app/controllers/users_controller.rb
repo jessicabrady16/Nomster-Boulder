d6191df69345eeb5ae6fb4e0f24d191976e919ae
Jessica Brady <jessicabrady16@gmail.com>
2019-08-15 12:28:01 -0600

    added profile info on user comment count and 5 star reviews and fixed minor CSS problems

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+    def show
+      @user = User.find(params[:id])
+      @user_5_star = @user.comments.where(rating: '5_stars').count
+      @comment_count = @user.comments.count
+  end
+end
