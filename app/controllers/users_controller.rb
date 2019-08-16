9a93ca6e91b43e3df1987cdc83ba73395bb4a61e
Jessica Brady <jessicabrady16@gmail.com>
2019-08-16 11:33:52 -0600

    added comment row and some css on profile page and added media query for smaller screens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def show
-      @user = User.find(params[:id])
-      @user_5_star = @user.comments.where(rating: '5_stars').count
-      @comment_count = @user.comments.count
+  def show
+    @user = User.find(params[:id])
+    @user_5_star = @user.comments.where(rating: '5_stars').count
+    @comment_count = @user.comments.count
   end
 end
